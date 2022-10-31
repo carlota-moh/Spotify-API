0db3919145054eef96b7906607566d4cbd9c5112
carlota <carlota.monederoh@gmail.com>
2022-10-31 17:19:37 +0100

    Turned to JN. Authorization and playlist creation working properly

--- get_token.py
+++ get_token.py
@@ -4,12 +4,32 @@ import base64
 import requests
 from requests.auth import HTTPBasicAuth
 
+# Request authorization from user
+
+auth_url = "https://accounts.spotify.com/authorize"
+redirect_uri = "https://jpn-top-100.com/callback"
+
+params = {"client_id": client_id,
+          "response_type": "code",
+          "redirect_uri": redirect_uri,
+          "scope": "playlist-modify-public"}
+
+response = requests.get(auth_url, params=params) 
+
+print("Please authorize app using this URL: " + response.url)
+redirect_response = input("\n\nPaste the URL you are being redirected to here: ")
+
+code = re.search("code(.*)", redirect_response)
+
+# Get access token
+
 token_url = "https://accounts.spotify.com/api/token"
 
 encoded64 = base64.b64encode((client_id + ":" + client_secret).encode("ascii")).decode("ascii")
 
 data = {
-     "grant_type": "client_credentials"
+     "grant_type": "authorization_code",
+
 }
 
 headers = {
