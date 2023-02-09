6a7c5dfe693f25fe855e761412d386062e0774db
Carlota <carlota.monederoh@gmail.com>
2023-02-09 15:46:15 +0100

    [playlists.py] make playslist func

--- playlists.py
+++ playlists.py
@@ -2,14 +2,29 @@ import json
 import base64
 import requests
 
-def call_endpoint(url, token):
+def get_request(url, token):
+
       headers = {"Authorization": "Bearer "+token,
                 "Content-Type": "application/json"}
 
       response = requests.get(url, headers=headers)
 
       if response.status_code == 200:
-            print("Successfully retrieved playlist")
+            print("Successful get request")
+            playlists = response.json()
+            return playlists
+      else:
+            print("Something went wrong! Response status code: %s" % response.status_code)
+
+def post_request(url, token, data):
+       
+      headers = {"Authorization": "Bearer "+token,
+                  "Content-Type": "application/json"}
+
+      response = requests.post(url, headers=headers, data=data)
+
+      if response.status_code == 200:
+            print("Successful post request")
             playlists = response.json()
             return playlists
       else:
@@ -18,7 +33,7 @@ def call_endpoint(url, token):
 def read_playlists(user_id, token):
       url = f"https://api.spotify.com/v1/users/{user_id}/playlists"
 
-      playlists = call_endpoint(url=url, token=token)
+      playlists = get_request(url=url, token=token)
 
       return playlists['items']
 
@@ -26,10 +41,19 @@ def read_playlist(user_id, token, playlist_id):
 
       url = f"https://api.spotify.com/v1/users/{user_id}/playlists/{playlist_id}"
 
-      playlist = call_endpoint(url=url, token=token)
+      playlist = get_request(url=url, token=token)
 
       return playlist
 
-def make_playlist():
-      pass
+def make_playlist(user_id, token, playlist_name, playlist_description = None):
+
+      url = f"https://api.spotify.com/v1/users/{user_id}/playlists"
+
+      data = json.dumps({"name": playlist_name,
+                        "description": playlist_description,
+                        "public": "true"}, indent=4)
+
+      post_request(url=url, token=token, data=data)
+
+
 
